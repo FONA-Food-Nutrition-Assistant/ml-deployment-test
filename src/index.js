e9a2666c9fde0da103374c1efea8651a07e3080a
gasous30 <youngblue.black@gmail.com>
2023-12-11 21:43:03 +0700

    add doPrediction function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
@@ -8,12 +9,13 @@ const logic = require('./ml-logic');
  * ML Model Implementation
  * @note This is a dummy implementation of the ML model
  */
-app.get('/predict', (req, res) => {
-	const calculationString = '1+1';
-	const result = logic.doPrediction(eval(calculationString), 2);
-
+app.post('/predict', bodyParser.json(), async (req, res) => {
+	// const calculationString = '1+1';
+	// const result = logic.doPrediction(eval(calculationString), 2);
+	const result = await logic.doPrediction(req.body.imgURL)
+	
 	res.status(200).json({
-		message: 'Hi There! We do some calculations here',
+		message: 'Give top 5 predictions:',
 		result: result,
 	});
 });
--- src/ml-logic.js
+++ src/ml-logic.js
@@ -1,12 +1,48 @@
-const tf = require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs')
+const tfn = require('@tensorflow/tfjs-node');
+const tfjs_conv = require('@tensorflow/tfjs-converter');
+var path = require('path');
+const canvass = require('canvas')
+
+TEST_IMG_URL = "https://storage.googleapis.com/fona-dev-bucket/foods/user-food-1.jpg"
+
+const MODEL_URL = tfn.io.fileSystem(path.join(__dirname, '..', 'from_saved_model', 'model.json'))
+
+const IMG_SIZE = 224
+
+const pred_class = require('../predClass.json')
+
+// async function loadModel(){
+// 	const model = await tf.loadGraphModel(MODEL_URL)
+// 	return model
+// }
 
 /**
  * Write your code below
  * @example
  */
-function doPrediction(calculations, result) {
-	const isResultCorrect = calculations === result;
-	return isResultCorrect;
+async function doPrediction(imgURL) {
+	// model = loadModel()
+	const model = await tf.loadGraphModel(MODEL_URL)
+
+	const imgElement = new canvass.createCanvas(IMG_SIZE, IMG_SIZE, 3)
+	
+	imgElement.src = imgURL
+
+	const img = tf.browser.fromPixels(imgElement).reshape([-1, IMG_SIZE, IMG_SIZE, 3]).toFloat()
+	
+	prediction = model.predict(img).dataSync()
+	
+	top5 = Array.from(prediction).map(function(p,i){
+		return {
+			probability: p,
+			className: pred_class[i]
+		};
+	}).sort(function(a,b){
+		return b.probability-a.probability;
+	}).slice(0,5)
+
+	return top5;
 }
 
 module.exports = {
